6750c4b02b3247887bb37d31494a75c23fac6915
chanjr227 <chandrajr227@gmail.com>
2025-08-02 16:28:34 +0700

    validasi hari

--- user/dashboard.php
+++ user/dashboard.php
@@ -1,3 +1,13 @@
 <?php
 require '../auth/session_check.php';
 require '../config/db.php';
+
+//cek waktu kehadiran
+$userId = $_SESSION['user_id'];
+$tanggalHariIni = date('Y-m-d');
+
+//ambil data hari ini
+$stmt = $koneksi->prepare("SELECT *  FROM absensi WHERE user_id = ?  AND tanggal = ?");
+$stmt->bind_param("is", $userId, $tanggalHariIni);
+$stmt->execute();
+$absenHariIni = $stmt->get_result()->fetch_assoc();
